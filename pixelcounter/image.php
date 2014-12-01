eb683c3597210d9bcae664d2f0b407eaa6d22489
Martin Kelm <martinkelm@shrt.ws>
2014-12-01 10:32:46 +0100

    ignore requests without referer

--- pixelcounter/image.php
+++ pixelcounter/image.php
@@ -6,22 +6,26 @@
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  * @copyright Copyright (c) 2014 Martin Kelm
  */
-include_once(__DIR__."/config.php");
-include_once(__DIR__."/classes/database.php");
-
 header('Content-Type: image/gif');
 echo base64_decode("R0lGODdhAQABAIAAAPxqbAAAACwAAAAAAQABAAACAkQBADs=");
 
-$db = new \PixelCounter\Database($config["database"], false);
-$cookieName = "idxpc_ip";
-if (!empty($_COOKIE[$cookieName])) {
-  $ip = $_COOKIE[$cookieName];
-} else {
-  $ip = $_SERVER["REMOTE_ADDR"];
+if (!empty($_GET["time"]) && !empty($_SERVER["HTTP_REFERER"])) {
+
+  include_once(__DIR__."/config.php");
+  include_once(__DIR__."/classes/database.php");
+
+  $db = new \PixelCounter\Database($config["database"], false);
+  $cookieName = "idxpc_ip";
+  if (!empty($_COOKIE[$cookieName])) {
+    $ip = $_COOKIE[$cookieName];
+  } else {
+    $ip = $_SERVER["REMOTE_ADDR"];
+  }
+  if ($db->checkIpVisit($ip, $config["iplifetime"]) == false) {
+    setcookie($cookieName, $ip, time() + $config["iplifetime"], "/");
+    $result = $db->insertVisit($ip);
+  }
+  $db->insertHit($ip);
+
 }
-if ($db->checkIpVisit($ip, $config["iplifetime"]) == false) {
-  setcookie($cookieName, $ip, time() + $config["iplifetime"], "/");
-  $result = $db->insertVisit($ip);
-}
-$db->insertHit($ip);
 
--- pixelcounter/test.php
+++ pixelcounter/test.php
@@ -1,1 +1,1 @@
-<img src="image.php?t=<?=microtime(true)?>" alt="" />
+<img src="image.php?time=<?=microtime(true)?>" alt="" />
