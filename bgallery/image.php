e369643460731bef25f2aec9013137d4f578807a
Martin Kelm <martinkelm@shrt.ws>
2014-12-03 13:08:36 +0100

    cache images

--- bgallery/image.php
+++ bgallery/image.php
@@ -6,15 +6,21 @@ if (empty($_SESSION["valid"]) || empty($_GET["iname"]) || empty($_GET["ifolder"]
 $imagesDir = __DIR__."/images/".$_GET["ifolder"].
   "/".substr($_GET["iname"], 0, 2)."/";
 $imageName = $_GET["iname"];
+$in = $imagesDir.$imageName;
 
-if (strpos($imageName, ".jpg") !== false || strpos($imageName, ".jpeg")) {
-  $type = "image/jpeg";
-} else if (strpos($imageName, ".png") !== false) {
-  $type = "image/png";
-} else if (strpos($imageName, ".gif") !== false) {
-  $type = "image/gif";
+if (isset($headers['If-Modified-Since']) &&
+    (strtotime($headers['If-Modified-Since']) == filemtime($in))) {
+  header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($in)).' GMT', true, 304);
+} else {
+  header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($in)).' GMT', true, 200);
+  if (strpos($imageName, ".jpg") !== false || strpos($imageName, ".jpeg")) {
+    $type = "image/jpeg";
+  } else if (strpos($imageName, ".png") !== false) {
+    $type = "image/png";
+  } else if (strpos($imageName, ".gif") !== false) {
+    $type = "image/gif";
+  }
+  header("Content-Type: ".$type);
+  header("Content-Length: ".filesize($in));
+  readfile($in);
 }
-
-header("Content-Type: ".$type);
-header("Content-Length: ".filesize($imagesDir.$imageName));
-readfile($imagesDir.$imageName);
